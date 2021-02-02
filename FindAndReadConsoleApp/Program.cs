09a3baf980acc850c064fab77c07c2386e05bf31
Victor Byliev <ya.vic4592@yandex.ru>
2021-02-02 13:18:08 +0300

    Add project files.

---
+++ FindAndReadConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace FindAndReadConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите имя файла");
+            var fileName = Console.ReadLine();
+            Console.WriteLine("Введите корневую папку");
+            var rootFolder = Console.ReadLine();
+            var filePath = rootFolder + fileName;
+            if (Directory.Exists(rootFolder))
+            {
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader streamReader = new StreamReader(filePath, System.Text.Encoding.Default) )
+                    {
+                        string line;
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+                else
+                { Console.WriteLine("Файл не найден"); }
+            }
+            else
+            {
+                Console.WriteLine("Заданной папки не существует");
+            }
+        }
+    }
+}
